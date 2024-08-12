a7b9ae5cace4a838adeac4f65fa8fe9c577d7fee
henryg123456 <159054664+henryg123456@users.noreply.github.com>
2024-08-12 13:54:03 -0300

    Add files via upload

---
+++ dump.sqlite (1).sql
@@ -0,0 +1,22 @@
+-- TABLE
+CREATE TABLE demo (ID integer primary key, Name varchar(20), Hint text );
+CREATE TABLE quadra(
+  ID INT (4),
+  cidade VARCHAR (30) UNIQUE,
+  capacidade VARCHAR (4) NOT NULL
+  
+ 
+  );
+CREATE TABLE Usuario(
+  ID INT (4),
+  NOME VARCHAR (30),
+  ESPORTE VARCHAR (30)
+  
+  );
+ 
+-- INDEX
+ 
+-- TRIGGER
+ 
+-- VIEW
+ 
